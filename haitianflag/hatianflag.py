efbd9fc07260238f2bbddb809b32df8bf3710e29
Makenson Noel <Makenson.noel@yahoo.com>
2021-05-21 02:15:00 -0400

    Add files via upload

---
+++ haitianflag/hatianflag.py
@@ -0,0 +1,78 @@
+"""Makenson I. Noel
+Develop With Mak
+May 18 2021 
+
+Created a program to generate the structure of a Haitian Flag"""
+
+from turtle import *
+
+
+def message():
+  for i in range(1):
+
+    color('white')
+    begin_fill()
+    forward(90)
+    right(90)
+    forward(90)
+    right(90)
+    forward(90)
+    right(90)
+    end_fill()
+
+    color('red')
+    begin_fill()
+    forward(45)
+    left(90)
+    forward(90)
+    left(90)
+    forward(90)
+    left(90)
+    forward(90)
+    left(90)
+    forward(45)
+    right(90)
+    forward(90)
+    right(90)
+    forward(45)
+    right(90)
+    forward(90)
+    right(180)
+    end_fill()
+
+    color('red')
+    forward(180)
+    begin_fill()
+    left(90)
+    forward(90)
+    left(90)
+    forward(90)
+    left(90)
+    forward(90)
+    end_fill()
+
+    right(180)
+    forward(90)
+
+    color('blue')
+
+    begin_fill()
+    right(90)
+    forward(90)
+    left(90)
+    forward(90)
+    left(90)
+    forward(270)
+
+    left(90)
+    forward(90)
+    left(90)
+    forward(90)
+    left(90)
+    forward(45)
+    right(90)
+    forward(90)
+    right(90)
+    end_fill()
+
+message()
