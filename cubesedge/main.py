44c6fa764c178c6d6ab8a754bdbc8bad7c27685d
Makenson Noel <Makenson.noel@yahoo.com>
2021-06-12 10:18:28 -0400

    Add files via upload

---
+++ cubesedge/main.py
@@ -0,0 +1,9 @@
+'''Makenson Noel
+   Program developed to calculate the surface area
+'''
+
+#Cube Input
+cube = int(input("Enter the cube's edge: "))
+
+#Output
+print("\nThe surface area",cube*24, "square units." )
