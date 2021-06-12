a9e990d8a78379ffb04f8080fb6a4db3285586c1
Makenson Noel <Makenson.noel@yahoo.com>
2021-06-12 10:18:12 -0400

    Add files via upload

---
+++ budgettool/main.py
@@ -0,0 +1,19 @@
+amount = float(input("Enter Amount: $"))
+
+print("Choose budget")
+
+def fft():
+
+  print("50/20/30")
+
+  ftt1 = (amount*0.50)
+  ftt2 = (amount*0.20)
+  ftt3 = (amount*0.30)
+
+  print("50% ", "$", ftt1)
+  print("20% ", "$", ftt2)
+  print("30% ", "$", ftt3)  
+
+choice1 = fft() 
+
+print(choice1)
