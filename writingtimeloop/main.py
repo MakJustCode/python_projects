2622481dc0527538af26edd2b933c6c51ac123d7
Makenson Noel <Makenson.noel@yahoo.com>
2021-06-12 10:19:49 -0400

    Add files via upload

---
+++ writingtimeloop/main.py
@@ -0,0 +1,14 @@
+print("Writing time") 
+
+print("Welcome")
+
+#Writing loops 
+
+#decisions 
+
+wjr = (input("whats happening...."))
+#whatshappening response whr
+i = 0 
+for i in range(5):
+  print("complete!, Thanks for your response")
+
