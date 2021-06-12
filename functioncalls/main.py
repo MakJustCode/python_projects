3103d1a0cedd730604b483c719662d8e406c4483
Makenson Noel <Makenson.noel@yahoo.com>
2021-06-12 10:18:51 -0400

    Add files via upload

---
+++ functioncalls/main.py
@@ -0,0 +1,17 @@
+'''Makenson Noel 
+   Global and Local variables
+'''
+global_var = 200 # a global variable 
+
+def func():
+  #local var is a local variable 
+  # and is only available inside func()
+  local_var = 100
+  print("Inside func() - local_var =", local_var)
+  #accessing a global variable inside a function 
+  print("Inside func() - global_var =", global_var)
+
+func()
+print("Outside func() - global_var =", global_var)
+#print("Outside func() - global_var =", global_var) #  ERROR: local_var is not available
+
