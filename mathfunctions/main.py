1db99c1f610512a38b7aa954d03f7268b7274837
Makenson Noel <Makenson.noel@yahoo.com>
2021-06-12 10:18:55 -0400

    Add files via upload

---
+++ mathfunctions/main.py
@@ -0,0 +1,14 @@
+'''Makenson Noel 
+   Math functions shown in python
+'''
+print("Quick Math")
+
+def addition(x,y):
+  return x+y; 
+
+print(addition(2,3))
+
+def subtraction(x,y):
+  return x-y; 
+
+print(subtraction(5,2))
