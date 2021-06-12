cedc1ff5b03fbe13e3fcb679f96277dddd0a901b
Makenson Noel <Makenson.noel@yahoo.com>
2021-06-12 10:17:52 -0400

    Add files via upload

---
+++ birthmonth/main.py
@@ -0,0 +1,11 @@
+'''Makenson Noel 
+Birth months'''
+
+
+#Array Of Months
+
+months = ["Blank", "Jan", "Feb", "Mar", "Apr", "Jun", "July", "Aug", "Sep", "Oct", "Nov", "Dec" ]
+
+print(months)
+
+birthmonth = input("Enter birthmonth ")
