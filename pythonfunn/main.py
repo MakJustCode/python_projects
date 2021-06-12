d1c7e2ad7d219d3df338d5db2b58273eb09ca9bf
Makenson Noel <Makenson.noel@yahoo.com>
2021-06-12 10:19:23 -0400

    Add files via upload

---
+++ pythonfunn/main.py
@@ -0,0 +1,17 @@
+'''Makenson Noel
+   Survey
+'''
+#Is programming fun 
+def fun():
+
+  print("Enter \"Yes\" or \"No\" ")
+  answer = input("Is programming fun ?: ")
+
+  if answer == "Yes" or answer == "yes": 
+    print("I agree ")
+
+  elif answer == "No" or answer == "no":
+    print("Sucks to be you XD")
+
+fun()
+  
