bfa2f2eb2409fb511402d4914b526a31e10f206e
Makenson Noel <Makenson.noel@yahoo.com>
2021-06-12 10:19:12 -0400

    Add files via upload

---
+++ pyarrays/main.py
@@ -0,0 +1,22 @@
+"""Makenson I Noel 
+   April 7th, 2021
+   Develope With Mak
+
+   Arrays and Loop"""
+
+#Function 
+def arrayLoop():
+    
+    #Array 
+    A = ["Jack", "And", "Jill", "Ran", "Up", "The", "Hill"]
+
+    #Length
+    length = len(A) 
+
+    #For Loop 
+    for i in range(length): 
+        print(A[i]) 
+
+print (arrayLoop()) 
+
+
